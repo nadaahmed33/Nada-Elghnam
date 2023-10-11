874b80014596cc284dd0ec547e3e2af960a2b54e
ziad-ezzat <ezzatziad83@gmail.com>
2023-10-12 00:24:01 +0200

    etit main and add some functions

--- nada.java
+++ nada.java
@@ -1,5 +1,30 @@
 public class nada{
     public static void main (String[] args){
+        nada nada = new nada();
+        nada.printName();
+        nada.printMajor();
+        nada.printYear();
+        nada.printHobbies();
+        nada.add(5, 6);
+    }
+
+    public void printName(){
         System.out.println("Nada Ahmed Elghnam");
+    }
+
+    public void printMajor(){
+        System.out.println("Computer Science");
+    }
+
+    public void printYear(){
+        System.out.println("Senior");
+    }
+
+    public void printHobbies(){
+        System.out.println("Reading, Writing, and Drawing");
+    }
+
+    public  void add(int a, int b){
+        System.out.println(a+b);
     }
 }
